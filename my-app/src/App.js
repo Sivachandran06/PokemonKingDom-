626f062f83e170043d55901084ccc76876bc449e
sivachandran06 <sivachandran997@gamil.com>
2024-05-16 16:20:37 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PokemonKingdomMainPage from './PokemonKingDom/PokemonKingdomMainPage'
+
+function App() {
+  return (
+    <div className="App">
+      <PokemonKingdomMainPage />
+    </div>
+  );
+}
+
+export default App;
